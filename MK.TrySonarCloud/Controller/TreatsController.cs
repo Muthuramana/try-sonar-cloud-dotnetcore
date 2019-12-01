66202b8b79a51acfbb8ada564f8074a4c281662c
Muthu <muthu@lightningcloudconsultancy.com>
2019-12-01 23:17:31 +0000

    initial commit

---
+++ MK.TrySonarCloud/Controller/TreatsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MK.TrySonarCloud.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TreatsController : ControllerBase
+    {
+        public IEnumerable<string> WhatCanIGet() => new string[] { "colouring", "lego", "ipad", "tv", "chocolates" };
+        public string Good(string action) => "lego";
+    }
+}
