c1ae999215700090ad1af1582d52e630a06a2e48
Muthu <muthu@lightningcloudconsultancy.com>
2019-12-01 23:40:48 +0000

    add test

---
+++ MK.TrySonarCloudUnitTests/Controller/TreatsControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MK.TrySonarCloud.Controller;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MK.TrySonarCloud.Controller.Tests
+{
+    [TestClass()]
+    public class TreatsControllerTests
+    {
+        [TestMethod()]
+        public void WhatCanIGetTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
